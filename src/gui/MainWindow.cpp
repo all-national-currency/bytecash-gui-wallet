ab7db47dabe5e36b8ed8d15143a8845e4a7abad7
CryptoGrams <crypto.grams@byte-cash.org>
2017-12-31 16:19:04 +0200

    Minor changes

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -1,6 +1,19 @@
-// Copyright (c) 2011-2015 The Cryptonote developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2018, The CryptoNote developers, The BYTECash developers
+//
+// This file is part of BYTECash.
+//
+// BYTECash is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BYTECash is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BYTECash. If not, see <https://opensource.org/licenses/lgpl-3.0/>.
 
 #include <QCloseEvent>
 #include <QFileDialog>
